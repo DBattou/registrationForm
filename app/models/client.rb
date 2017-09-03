8b8a10c36af260014cbdf8872128766e6bc68991
Battou <battou@MacBook-Pro-de-Camille.local>
2017-09-03 19:37:56 +0200

    handle invalid name and existing name as separate cases

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,8 +1,26 @@
 class Client < ApplicationRecord
-	validates :name,
-		presence: true,
-		uniqueness: true,
-		length: { is: 3 },
-		format: { without: /[^a-z]/ }
+  before_save :valid_uniqueness
+  validates :name,
+    presence: true,
+    length: { is: 3 },
+    format: { without: /[^a-z]/ }
 
+    # Check if record exists in the Client table
+    def valid_uniqueness
+      throw :abort if is_unique? == false
+    end
+
+    # Check if record exists in the Client table
+    def is_unique?
+      if Client.exists?(name: self.name)
+        false
+      else
+        true
+      end
+    end
+
+    # Generate new name if entry already exists
+    def generate_random_name
+      ('a'..'z').to_a.shuffle[0,3].join
+    end
 end
